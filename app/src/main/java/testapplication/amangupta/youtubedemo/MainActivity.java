abe41b4f6cc82a2ed348f4fe382dfec82b1e92ce
aman gupta <aman.gupta39@webkul.com>
2016-06-06 15:01:11 +0530

    First commit

---
+++ app/src/main/java/testapplication/amangupta/youtubedemo/MainActivity.java
@@ -0,0 +1,159 @@
+package testapplication.amangupta.youtubedemo;
+
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
+import java.util.List;
+
+public class MainActivity extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener, View.OnClickListener {
+    private static final String DEVELOPER_KEY = "AIzaSyBfeip_1VzrFATTbRJvjyqpy2Vsw6Mm_XE";
+    private static final String VIDEO_ID = "cdgQpa1pUUE";
+    private Button playVideoButton;
+    private CheckBox lightboxModeCheckBox;
+    private static final int REQ_START_STANDALONE_PLAYER = 1;
+    private static final int REQ_RESOLVE_SERVICE_MISSING = 2;
+    private YouTubePlayerView youTubeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        playVideoButton = (Button) findViewById(R.id.start_video_button);
+        lightboxModeCheckBox = (CheckBox) findViewById(R.id.lightbox_checkbox);
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+
+        // Initializing video player with developer key
+        youTubeView.initialize(DEVELOPER_KEY, this);
+        playVideoButton.setOnClickListener(this);
+//        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, REQ_START_STANDALONE_PLAYER).show();
+        } else {
+            String errorMessage = String.format(
+                    getString(R.string.error_player), errorReason.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                        YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+
+            // loadVideo() will auto play video
+            // Use cueVideo() method, if you don't want to play it automatically
+            player.cueVideo(VIDEO_ID);
+
+            // Hiding player controls
+//            player.setPlayerStyle(YouTubePlayer.PlayerStyle.CHROMELESS);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        boolean lightboxMode = lightboxModeCheckBox.isChecked();
+
+        Intent intent = null;
+        if (v == playVideoButton) {
+            intent = YouTubeStandalonePlayer.createVideoIntent(
+                    this, DEVELOPER_KEY, VIDEO_ID, 0, false, lightboxMode);
+        }
+
+        if (intent != null) {
+            if (canResolveIntent(intent)) {
+                startActivityForResult(intent, REQ_START_STANDALONE_PLAYER);
+            } else {
+                // Could not resolve the intent - must need to install or update the YouTube API service.
+                YouTubeInitializationResult.SERVICE_MISSING
+                        .getErrorDialog(this, REQ_RESOLVE_SERVICE_MISSING).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ_START_STANDALONE_PLAYER) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(DEVELOPER_KEY, this);
+        }
+        if (requestCode == REQ_START_STANDALONE_PLAYER && resultCode != RESULT_OK) {
+            YouTubeInitializationResult errorReason =
+                    YouTubeStandalonePlayer.getReturnedInitializationResult(data);
+            if (errorReason.isUserRecoverableError()) {
+                errorReason.getErrorDialog(this, 0).show();
+            } else {
+                String errorMessage =
+                        String.format(getString(R.string.error_player), errorReason.toString());
+                Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    private boolean canResolveIntent(Intent intent) {
+        List<ResolveInfo> resolveInfo = getPackageManager().queryIntentActivities(intent, 0);
+        return resolveInfo != null && !resolveInfo.isEmpty();
+    }
+
+    private int parseInt(String text, int defaultValue) {
+        if (!TextUtils.isEmpty(text)) {
+            try {
+                return Integer.parseInt(text);
+            } catch (NumberFormatException e) {
+                // fall through
+            }
+        }
+        return defaultValue;
+    }
+
+    private YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtube_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
